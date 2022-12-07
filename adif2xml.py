2ddaf3460d4d509130e9fe60d8acdc3fc002c5f2
mbridak <michael.bridak@gmail.com>
2022-12-07 08:21:02 -0800

    Added command line arguments.

--- adif2xml.py
+++ adif2xml.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """
 ADIF 2 XML
 
@@ -5,12 +7,38 @@ Get the lib adif_io from pypi
 pip3 install adif-io
 
 """
-
+import sys
 from xml.dom.minidom import parseString
-from dicttoxml import dicttoxml
-import adif_io
+import argparse
 
-qsos_raw, adif_header = adif_io.read_from_file("QSO.adi")
+try:
+    from dicttoxml import dicttoxml
+except ModuleNotFoundError:
+    print("Library dictoxml Not Found. You can get it with: pip3 install dictoxml")
+    sys.exit()
+
+try:
+    import adif_io
+except ModuleNotFoundError:
+    print("Library adif_io Not Found. You can get it with: pip3 install adif-io")
+    sys.exit()
+
+parser = argparse.ArgumentParser(
+    prog="adif2xml.py", description="Converts an ADIF file to XML"
+)
+
+parser.add_argument("filename")
+parser.add_argument(
+    "-o", "--output", default="QSO.xml", type=argparse.FileType("w", encoding="UTF-8")
+)
+
+args = parser.parse_args()
+
+if not args.filename:
+    print("No filename given. Please provide a filename.")
+    sys.exit()
+
+qsos_raw, adif_header = adif_io.read_from_file(args.filename)
 Qs = {}
 
 
@@ -19,5 +47,4 @@ for q, contact in enumerate(qsos_raw):
 
 XML_TO_OUTPUT = dicttoxml(Qs, attr_type=False, custom_root="CONTACTS")
 
-with open("QSO.xml", "w", encoding="UTF-8") as xmlfile:
-    xmlfile.write(parseString(XML_TO_OUTPUT).toprettyxml())
+args.output.write(parseString(XML_TO_OUTPUT).toprettyxml())
